82d23eba189838d1e027c3f381e318cc29d0a85b
CivilTT <89191801+CivilTT@users.noreply.github.com>
2025-11-21 18:45:00 +0000

    🚨 Apply Ruff Formatting Change

--- src/common/scripts/types/parser.py
+++ src/common/scripts/types/parser.py
@@ -11,7 +11,7 @@ def safe_parse[T: BaseModel](
   Pydanticによって実装された型定義によって，内容のバリデーションを安全に行う
   """
   try:
-    parsed:T = type_obj.model_validate(kwargs)
+    parsed: T = type_obj.model_validate(kwargs)
     return Success(parsed)
   except ValidationError as e:
     return Failure(e)
